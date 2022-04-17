37abd69996a20b12c5119e19184d2b4065515bca
Aaron Gonzalez <aaronraulgonzalez@gmail.com>
2022-04-16 20:17:56 -0500

    Almost Done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import "./Contact.css"
+import {MdEmail} from "react-icons/md"
+import emailjs from "emailjs-com"
 
 function Contact() {
+  const form =useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_8nwzfct', 'template_r4lnb0c', form.current, 'fQdXMxUljRXt5R34L')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact-container">
+        <div className="contact-options">
+          <article className="contact-option">
+            <MdEmail className="contact-option-icon"/>
+            <h4>Email</h4>
+            <h5>email@email.com</h5>
+            <a href="mailto:email@email.com">Send Email</a>
+          </article>
+          <article className="contact-option">
+            <MdEmail className="contact-option-icon"/>
+            <h4>Message</h4>
+            <h5>123-456-1234</h5>
+            <a href="mailto:email@email.com">Send Message</a>
+          </article>
+
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+            <input type="text" name="name" placeholder="Full Name" required />
+            <input type="email" name="email" placeholder="Email" required />
+            <textarea name="message" rows="7" placeholder="Message" required></textarea>
+            <button type="submit" className="btn btn-primary">Send Message</button>
+          </form>
+      </div>
+    </section>
   )
 }
 
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./Footer.css"
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer-logo">Aaron</a>
+      <ul className="permalinks">
+        <li><a href="#"></a>Home</li>
+        <li><a href="#"></a>About</li>
+        <li><a href="#"></a>Experience</li>
+        <li><a href="#"></a>Portfolio</li>
+        <li><a href="#"></a>Contact</li>
+      </ul>
+
+      <div className="footer-socials">
+        <a href="https://github.com/"><FaGithub /></a>
+        <a href="https://linkedin.com/"><BsLinkedin /></a>
+      </div>
+    </footer>
   )
 }
 
